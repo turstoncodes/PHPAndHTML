c3161cbfaae2b5a3f69961e67c8f3ac8590d8fdc
turstoncodes <53752666+turstoncodes@users.noreply.github.com>
2019-08-26 11:29:48 +0200

    Add files via upload

---
+++ Introduction.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang=en>
+<head>
+<meta charset=utf-8>
+<title>A Static Site</title>
+</head>
+<body>
+  <h1>Static?</h1>
+  <p>We call this type of site static, because it never changes.</p>
+  <p>Every time you visit this site, you see the same thing. If it was located on a public website, other users would also see the same thing.</p>
+</body>
+</html>
